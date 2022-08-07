e3a52affc80eff5ba84f64501bcd866297ebcc22
manodecristiano <cristian.carre.online@gmail.com>
2022-08-07 21:57:20 +0200

    Completed only animations

--- docs/js/animations.js
+++ docs/js/animations.js
@@ -1,9 +1,9 @@
 gsap.registerPlugin(ScrollTrigger);
 
 
-gsap.to('#movil__animated',{
+gsap.to('#movil__animated2',{
    duration:15,
-   x:930,
+   x:130,
    ease:'power3',
    scrollTrigger:"#movil__animated"
 
